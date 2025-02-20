97f1740435cbde02b716e11d6a98584bbc6f85ad
hossam6854 <150062047+hossam6854@users.noreply.github.com>
2025-02-20 11:57:27 +0200

    Updates

--- assets/index-CVPWxGhV.js
+++ assets/index-CVPWxGhV.js
@@ -1,4 +1,4 @@
-import{r as q}from"./index-B72p1ajp.js";/**
+import{r as q}from"./index-CtmeOfDN.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
